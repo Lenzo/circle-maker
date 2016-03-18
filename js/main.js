c75387ffb58284badc0b8c03d2d3aa48d3c7fad7
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-03-18 11:31:24 -0400

    indentation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
-var $body = $('body')
+var $body = $('body');
 
-var makeCircle = function() {
-	var $newCircle = $('<div>');
-	var newRed = Math.floor(Math.random() * 256);
-	var newGreen = Math.floor(Math.random() * 256);
-	var newBlue = Math.floor(Math.random() * 256);
-	var newTrans = Math.random();
-	var newSize = Math.random() * 20;
+var makeCircle = function () {
+  var $newCircle = $('<div>');
+  var newRed = Math.floor(Math.random() * 256);
+  var newGreen = Math.floor(Math.random() * 256);
+  var newBlue = Math.floor(Math.random() * 256);
+  var newTrans = Math.random();
+  var newSize = Math.random() * 20;
 
+  $newCircle.css('top', Math.random() * document.documentElement.clientHeight - 22);
+  $newCircle.css('left', Math.random() * document.documentElement.clientWidth - 22);
+  $newCircle.css('background-color', 'rgb(' + newRed + ', ' + newGreen + ', ' + newBlue + ')');
+  $newCircle.css('opacity', newTrans);
+  $newCircle.css('width', newSize);
+  $newCircle.css('height', newSize);
+  $newCircle.addClass('ball');
 
-	$newCircle.css('top', Math.random() * document.documentElement.clientHeight - 22);
-	$newCircle.css('left', Math.random() * document.documentElement.clientWidth - 22);
-	$newCircle.css('background-color', 'rgb(' + newRed + ', ' + newGreen + ', ' + newBlue + ')');
-	$newCircle.css('opacity', newTrans);
-	$newCircle.css('width', newSize);
-	$newCircle.css('height', newSize);
-
-
-	$body.append($newCircle);
+  $body.append($newCircle);
 };
 
-$('html').on('keydown', function(e) {
+$('html').on('keydown', function (e) {
 
-	var numCircles = Math.floor(Math.random() * 25);
-	var i = 0;
+  var numCircles = Math.floor(Math.random() * 25);
+  var i = 0;
 
-	for (i=0; i<numCircles; i++) {
-		makeCircle();
-	}
-
+  for (i = 0; i < numCircles; i++) {
+    makeCircle();
+  }
 
 });
-
